1fb6f62089c7f549c7835293ed8327b0ffdad617
Kolkirill <119480836+Kolkirill@users.noreply.github.com>
2022-11-30 10:00:37 +0300

    Add files via upload

---
+++ easy (1).h
@@ -0,0 +1,9 @@
+#ifndef EASY_H_INCLUDED
+#define EASY_H_INCLUDED
+
+string itc_hello_str(string name);
+
+#endif // EASY_H_INCLUDED
+
+
+
---
+++ fucns.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+
+string itc_hello_str(string name)
+{
+    string str = "Hello";
+    str += name
+    return str;
+}
